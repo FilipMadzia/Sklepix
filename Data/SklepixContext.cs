726ec7864ceb9e18fada69f07b62dd423604745c
Filip Madzia <filipmadzia33@gmail.com>
2023-07-06 19:31:46 +0200

    Dodaj pliki projektów.

---
+++ Data/SklepixContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sklepix.Models;
+
+namespace Sklepix.Data
+{
+    public class SklepixContext : DbContext
+    {
+        public SklepixContext (DbContextOptions<SklepixContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sklepix.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sklepix.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        [Required(ErrorMessage = "Nazwa produktu jest wymagana")]
+        public string? ProductName { get; set; }
+        [Required(ErrorMessage = "Cena jest wymagana")]
+        [RegularExpression(@"\d{1,9}[\.,\,]\d{2}", ErrorMessage = "Podaj właściwą cenę")]
+        public decimal ProductPrice { get; set; }
+
+        public Product()
+        {
+            
+        }
+    }
+}
