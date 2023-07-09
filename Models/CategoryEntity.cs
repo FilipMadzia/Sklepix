2b38722d2e0cac24dfb5833d37dcda9877255fa7
Filip Madzia <filipmadzia33@gmail.com>
2023-07-09 20:21:38 +0200

    Small code cleanup

--- Models/CategoryEntity.cs
+++ Models/CategoryEntity.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Sklepix.Models
 {
     public class CategoryEntity
     {
         public int Id { get; set; }
+        [DisplayName("Nazwa")]
         public string? Name { get; set; }
     }
 }
--- Models/ProductEntity.cs
+++ Models/ProductEntity.cs
@@ -1,12 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Sklepix.Models
 {
     public class ProductEntity
     {
         public int Id { get; set; }
+        [DisplayName("Nazwa")]
         public string? Name { get; set; }
+        [DisplayName("Cena")]
         public decimal Price { get; set; }
+        [DisplayName("Kategoria")]
         public CategoryEntity? Category { get; set; }
     }
 }
