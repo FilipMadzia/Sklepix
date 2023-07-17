05889020559bedca4926b44f47ccac7243926dec
Filip Madzia <filipmadzia33@gmail.com>
2023-07-17 23:21:57 +0200

    Statistics

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Sklepix.Models;
+using Sklepix.Repositories;
 using System.Diagnostics;
 
 namespace Sklepix.Controllers
@@ -7,15 +8,31 @@ namespace Sklepix.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly ProductRepository _productRepository;
+		private readonly CategoryRepository _categoryRepository;
+		private readonly AisleRepository _aisleRepository;
+		private readonly ShelfRepository _shelfRepository;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, ProductRepository productRepository, CategoryRepository categoryRepository, AisleRepository aisleRepository, ShelfRepository shelfRepository)
 		{
 			_logger = logger;
+			_productRepository = productRepository;
+			_categoryRepository = categoryRepository;
+			_aisleRepository = aisleRepository;
+			_shelfRepository = shelfRepository;
 		}
 
 		public IActionResult Index()
 		{
-			return View();
+			HomeViewModel model = new HomeViewModel()
+			{
+				ProductCount = _productRepository.GetProducts().Count,
+				CategoryCount = _categoryRepository.GetCategories().Count,
+				AisleCount = _aisleRepository.GetAisles().Count,
+				ShelfCount = _shelfRepository.GetShelves().Count
+			};
+
+			return View(model);
 		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Sklepix.Models
+{
+	public class HomeViewModel
+	{
+		[DisplayName("Ilość produktów")]
+		public int ProductCount { get; set; }
+		[DisplayName("Ilość kategorii")]
+		public int CategoryCount { get; set; }
+		[DisplayName("Ilość alejek")]
+		public int AisleCount { get; set; }
+		[DisplayName("Ilość półek")]
+		public int ShelfCount { get; set; }
+	}
+}
