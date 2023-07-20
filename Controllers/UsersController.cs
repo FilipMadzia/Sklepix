749c1307010b6b5d18f8387ff5866ae3e302839c
Filip Madzia <filipmadzia33@gmail.com>
2023-07-20 21:47:03 +0200

    Widok wszystkich pracowników

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Sklepix.Data.Entities;
+using Sklepix.Models;
+
+namespace Sklepix.Controllers
+{
+	[Authorize(Roles = "Administrator")]
+	public class UsersController : Controller
+	{
+		UserManager<UserEntity> _userManager;
+
+		public UsersController(UserManager<UserEntity> userManager)
+		{
+			_userManager = userManager;
+		}
+		public IActionResult Index()
+		{
+			List<UserViewModel> userVms = _userManager.Users
+				.Select(i => new UserViewModel()
+				{
+					Id = i.Id,
+					UserName = i.UserName,
+					Email = i.Email,
+					PhoneNumber = i.PhoneNumber
+				})
+				.ToList();
+
+			return View(userVms);
+		}
+	}
+}
---
+++ Models/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace Sklepix.Models
+{
+	public class UserViewModel
+	{
+		public string? Id { get; set; }
+		[DisplayName("Data dodania")]
+		public DateTime CreationTime { get; set; }
+		[DisplayName("Imię")]
+		public string? UserName { get; set; }
+		[DisplayName("Email")]
+		public string? Email { get; set; }
+		[DisplayName("Nr telefonu")]
+		public string? PhoneNumber { get; set; }
+	}
+}
